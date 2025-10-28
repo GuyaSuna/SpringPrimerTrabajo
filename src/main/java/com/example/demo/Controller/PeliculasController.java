2c406afe7bb2f690bbf790d5f7081ea0170c5eb9
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2025-10-27 22:15:02 -0300

    ejercicios de clase

--- src/main/java/com/example/demo/Controller/PeliculasController.java
+++ src/main/java/com/example/demo/Controller/PeliculasController.java
@@ -1,23 +1,56 @@
 package com.example.demo.Controller;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.PeliculaEntity;
 
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@RequestMapping("Pelicula")
 public class PeliculasController {
     
-    static ArrayList<String> peliculas = new ArrayList<String>();
+
+    static ArrayList<PeliculaEntity> listaPeliculas = new ArrayList<>();
+
+
+    @GetMapping("getPelicula/{id}")
+    public PeliculaEntity getPelicula(@PathVariable int id) {
+
+        PeliculaEntity peliculaResp = null;
+
+        for(PeliculaEntity pelicula : listaPeliculas){
+            if(pelicula.getId() == id){
+                peliculaResp = pelicula;
+            }
+        }
+        return  peliculaResp;
+    }
+    
+
 
     @GetMapping()
     public String getMethodName() {
         return "Api Funcional";
     }
+
+    @GetMapping("altaPelicula/{id}/{nombre}")
+    public ArrayList<PeliculaEntity> altaPelicula(@PathVariable int id , @PathVariable String nombre) {
+        PeliculaEntity pelicula = new PeliculaEntity(id, nombre);
+        listaPeliculas.add(pelicula);
+        return listaPeliculas;
+    }
     
 
+    @GetMapping("getUsuario/{id}")
+    public String getMethodName(@RequestParam String param , @PathVariable int id) {
+        return "Si funciona: " + param + " " + id;
+    }
+    
 
 }
--- src/main/java/com/example/demo/Entity/PeliculaEntity.java
+++ src/main/java/com/example/demo/Entity/PeliculaEntity.java
@@ -3,9 +3,36 @@ package com.example.demo.Entity;
 public class PeliculaEntity {
     
 
+private int id;
+
+private String nombre;
+
+public int getId(){
+    return this.id;
+}
+
+public String getNombre(){
+    return this.nombre;
+}
+
+public void setId(int id){
+    this.id = id;
+} 
+
+public void setNombre(String nombre){
+    this.nombre = nombre;
+}
 
 
+@Override
+public String toString(){
+    return "Pelicula: "+ this.id  + " " + this.nombre;
+}
 
+public PeliculaEntity(int id , String nombre){
+    this.id = id;
+    this.nombre = nombre;
+}
 
 
     
